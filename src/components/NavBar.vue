d3424dae82513cb6bbb213bdc6eef1cb5b76b974
Nicolas Duplaix <nicolas@widop.com>
2019-12-05 18:26:19 +0100

    [FEAT] add nav bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="nav-bar">
+<!--    <div class="nav-bar__logo"><img src="http://placehold.it/30x30" alt=""></div>-->
+    <div class="nav-bar__content">
+      <div class="nav-bar__content__item">Notre entreprise</div>
+      <div class="nav-bar__content__item">nos projets</div>
+      <div class="nav-bar__content__item">nous contacter</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+  .nav-bar {
+    position: sticky;
+    z-index: 99999;
+    top:0;
+    display: flex;
+    align-items: center;
+    height: 75px;
+    background-color: gray;
+    &__logo {
+      width: 100px;
+      height: 100px;
+
+      img {
+        width: 100%;
+      }
+    }
+     &__content {
+       width: 100%;
+       display: flex;
+       justify-content: space-around;
+       &__item {
+         text-align: center;
+       }
+     }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav-bar/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, debitis delectus
+      doloremque exercitationem id in quae quod ratione sapiente? Inventore, pariatur, quis. Aperiam
+      architecto at qui sapiente vel? Deleniti, ex.</p>
+    <p>Aspernatur at aut consequatur dignissimos dolorem ducimus facere harum impedit, nesciunt
+      officia omnis provident rem saepe unde vitae voluptas voluptatem. Consequuntur fugiat, hic
+      optio quae quos recusandae reiciendis voluptatum. Omnis!</p>
+    <p>Ab aliquid aut blanditiis consequatur deserunt dolor dolores enim eveniet id illo magnam
+      maxime odit, omnis porro possimus praesentium quas qui, quidem quisquam quod, quos ratione
+      reiciendis repudiandae ut vel.</p>
+    <p>Accusantium doloribus eius excepturi exercitationem labore magni quam quibusdam, quod
+      voluptates. Aliquid aspernatur doloremque est magnam. Earum illo, nesciunt perspiciatis quae
+      quasi qui tenetur! A illum nesciunt quibusdam reiciendis sit.</p>
+    <p>A aliquam animi aperiam at dicta doloremque ducimus ea eum facilis fugit maiores minus
+      molestias necessitatibus nesciunt, omnis quas quod rem repellat repudiandae sed sit temporibus
+      tenetur vel vero voluptatum.</p>
+    <p>Architecto dicta dolor earum eum, harum hic minima mollitia nobis qui quibusdam. Consequatur
+      culpa eveniet, ex iusto molestias nam natus nihil optio perferendis quaerat quisquam
+      reprehenderit sapiente similique sint voluptate.</p>
+    <p>Accusantium cumque et fugit illum in perspiciatis sequi, similique! Aliquam architecto ea
+      eaque impedit omnis repellendus tempore! Amet dolorum, iusto laudantium nesciunt nisi non
+      omnis pariatur, possimus repudiandae soluta voluptates.</p>
+    <p>Alias animi architecto at blanditiis cumque delectus eum fuga in iste, itaque iure, labore
+      laudantium maxime nesciunt odio omnis possimus provident quae quod reiciendis repellat rerum
+      sunt temporibus ullam velit.</p>
+    <p>Ex officiis optio rerum voluptates? Amet commodi dolor excepturi fugit impedit incidunt,
+      itaque laborum minus nemo obcaecati, odit officia officiis pariatur possimus reprehenderit
+      sint temporibus totam ullam unde vel voluptatum!</p>
+    <p>Adipisci eius fugit laboriosam suscipit totam. A adipisci, alias assumenda cupiditate
+      deleniti eligendi facilis illum ipsa laudantium libero maiores neque nulla pariatur
+      perspiciatis placeat repudiandae saepe sequi suscipit temporibus unde!</p>
+    <p>A, accusantium aut beatae cum, dicta dignissimos doloremque doloribus esse ipsa iure
+      laudantium, maiores maxime molestiae nam nisi odio quisquam repellendus repudiandae rerum sed
+      suscipit tenetur ullam unde vero voluptas?</p>
+    <p>Aliquam amet, animi asperiores cum dolor eligendi excepturi facilis fuga iusto libero magni
+      maxime molestiae nobis non odit officia optio perspiciatis porro quam quasi quisquam quo
+      similique totam veritatis, vero?</p>
+    <p>Animi aspernatur dolore error excepturi facilis ullam velit, vero. Dolore impedit minima
+      nobis sed tenetur voluptatem. Accusamus debitis dolore dolorum harum, quibusdam sequi tenetur.
+      Dolores error ipsa itaque maxime repudiandae.</p>
+    <p>Aliquid laudantium molestias repellendus! Perferendis recusandae, sapiente. Adipisci, aut
+      blanditiis commodi consequatur culpa ducimus earum eius et fuga iste libero minima pariatur,
+      porro quasi qui quia rerum sed, unde velit?</p>
+    <p>Adipisci aperiam aut cumque facere, hic incidunt iure laborum magni saepe vel? Cumque
+      explicabo nulla reiciendis! Blanditiis commodi consequuntur cumque in iure, non odio officiis
+      sint tempore voluptatum? Accusamus, ipsam!</p>
+    <p>Cupiditate dolores nisi perferendis quam tempora? Alias asperiores at autem beatae blanditiis
+      corporis, culpa dignissimos doloribus est illo in ipsam maxime minima perspiciatis porro quae
+      repudiandae, rerum sunt tempore voluptas!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda distinctio est inventore
+      odit officia? A ab autem corporis, cum dolorum iure nesciunt perferendis quidem quos
+      recusandae suscipit tempora veniam voluptas.</p>
+    <p>Ad autem, consequuntur deleniti dolor dolore dolorum inventore iure labore neque nisi odio
+      quam quia, quidem reiciendis saepe, tempore unde? Corporis culpa debitis neque nisi nobis
+      repellat, sapiente. Id, sint?</p>
+    <p>Accusamus accusantium aliquid, atque aut doloribus ex fugit hic iusto laudantium nostrum,
+      numquam omnis provident, quos? Aliquid aperiam deserunt eligendi hic maxime odit sed unde
+      voluptatem voluptatum! Ipsum modi, tempore.</p>
+    <p>Accusamus accusantium amet assumenda autem consequuntur culpa cupiditate dicta dolore ducimus
+      eius eveniet fuga in, ipsa labore minima molestias necessitatibus nulla officia omnis
+      perferendis quos repellat repellendus reprehenderit vero voluptatibus?</p>
+    <p>Accusantium animi asperiores aspernatur blanditiis cum delectus dolorum ducimus eos esse est
+      eveniet ex, excepturi explicabo facilis itaque iusto laboriosam maiores natus nobis nostrum
+      odio optio pariatur quaerat reiciendis repellendus.</p>
+    <p>Accusamus animi architecto autem culpa doloribus, ducimus ea, ex harum illum iure laudantium
+      magni necessitatibus numquam optio perspiciatis possimus, quasi quis rerum veritatis vero.
+      Eaque expedita inventore iure sint unde!</p>
+    <p>Asperiores aspernatur distinctio dolorem esse illo ipsum necessitatibus recusandae ut! Beatae
+      eligendi maxime mollitia, nam natus omnis quaerat quam? Amet consequuntur dolore eligendi,
+      fugiat magni modi molestias necessitatibus sapiente ut.</p>
+    <p>A accusantium animi blanditiis consequatur corporis excepturi labore nam, perferendis
+      possimus provident quae quod quos reiciendis reprehenderit sit tempore ullam! Blanditiis
+      excepturi inventore libero, officiis optio qui quisquam repellendus sunt?</p>
+    <p>Alias dolorem ex hic nobis quibusdam? Ab aperiam, asperiores aspernatur at consequatur
+      dignissimos ea error esse expedita hic impedit laborum maiores perspiciatis quas ratione
+      repellat sequi suscipit velit voluptas voluptate?</p>
+    <p>Architecto cupiditate, dignissimos dolor doloremque ex fugiat inventore iste molestias, nihil
+      quae quo quod repudiandae. Accusamus accusantium alias dicta, eligendi explicabo fuga itaque
+      pariatur sapiente. Aliquid nostrum quia quidem sint.</p>
+    <p>Accusantium, adipisci amet aperiam architecto blanditiis, cupiditate dignissimos dolorem
+      doloremque, esse et explicabo facere impedit labore modi molestias necessitatibus nostrum
+      nulla officia omnis perferendis quod quos sed. Expedita, fuga inventore?</p>
+    <p>Accusamus ad alias aperiam consectetur cum deserunt dolor doloribus eaque esse est eveniet ex
+      fugit ipsa iste itaque iure magnam maiores quae, repellendus repudiandae temporibus velit
+      veniam veritatis voluptate voluptates?</p>
+    <p>Aliquam animi, ex fuga magnam nobis numquam, pariatur provident quae quia quisquam sapiente
+      unde vel vero. Commodi dolor fuga maiores neque nulla reiciendis? Excepturi, harum placeat
+      quia quibusdam reiciendis saepe!</p>
+    <p>Architecto, cumque dolor dolorem eum fugit id labore laborum minus quisquam repudiandae
+      sapiente ullam voluptas voluptatem? Aliquid, ducimus eius error iste, omnis optio perferendis
+      quia, quisquam quos rerum tenetur totam?</p>
+    <p>Alias assumenda commodi corporis dicta dignissimos dolor eaque fugiat harum in incidunt
+      inventore ipsum labore minima nobis non obcaecati odit quae rem, sequi sint! At illum repellat
+      reprehenderit saepe sed!</p>
+    <p>Accusamus blanditiis culpa cumque delectus eius facilis harum id ipsum maiores minima minus
+      molestiae nam non nulla odio perferendis quisquam sed suscipit totam, vero! Error excepturi
+      illum sed totam voluptas.</p>
+    <p>Alias asperiores corporis cupiditate distinctio facere ipsum magnam, neque odit saepe
+      veritatis! A atque error facilis fuga, laboriosam nemo nostrum omnis optio perspiciatis
+      quibusdam reiciendis repellendus rerum sapiente veniam voluptate.</p>
+    <p>Amet animi consequuntur delectus dolor, dolores enim eos et ex exercitationem, illum itaque
+      nihil tempora ullam ut voluptatem. Accusantium alias dicta odio placeat vitae? Assumenda culpa
+      cum fugiat optio tempore!</p>
+    <p>Asperiores assumenda distinctio eveniet illo laboriosam molestiae, mollitia obcaecati sed ut.
+      Architecto aspernatur deserunt dicta distinctio dolor ea eaque enim esse necessitatibus nobis
+      officia quasi quidem, rem repudiandae sint totam.</p>
+    <p>Aperiam consequuntur cum dignissimos distinctio ducimus eaque harum incidunt ipsum magnam
+      modi nisi perspiciatis recusandae repellat suscipit, unde vel voluptatum. Dolores expedita
+      ipsam molestiae mollitia quaerat reprehenderit saepe sequi voluptatibus.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import NavBar from '../components/NavBar';
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld,
+    NavBar,
   },
 };
 </script>
