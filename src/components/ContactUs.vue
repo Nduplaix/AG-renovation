5cc974b4025f27549b24dc1c37e7a0185993b532
Nicolas Duplaix <nicolas@widop.com>
2020-01-24 07:06:00 +0100

    [FEAT] send Mail

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="contact" class="contact">
+    <validation-popin type="success" @close="displayValidPopin = false" v-if="displayValidPopin">
+      Votre mail a bien été envoyé ! <br>Merci
+    </validation-popin>
+    <validation-popin type="warning" @close="displayErrorPopin = false" v-if="displayErrorPopin">
+      Il y a eu une erreur dans l'envoie de votre mail !
+    </validation-popin>
     <div class="contact__info">
       <p class="h2">Angelo Opigez - Gregory Caron</p>
       <p>mail: contact@agrenovation.com</p>
@@ -8,7 +14,7 @@
     <img src="../assets/images/logo.png" alt="" class="contact__logo">
     <div class="contact__form">
       <p class="h1">Nous contacter</p>
-      <form action="#">
+      <form @submit.prevent="sendMail">
         <div class="form-group">
           <label for="contact--email">Votre adresse e-mail</label>
           <input type="email" id="contact--email" class="form-control" v-model="email">
@@ -26,12 +32,44 @@
 </template>
 
 <script>
+import axios from 'axios';
+import ValidationPopin from './popin/ValidationPopin.vue';
+
 export default {
+  components: {
+    ValidationPopin,
+  },
   data() {
     return {
       email: '',
       mailContent: '',
+      displayValidPopin: false,
+      displayErrorPopin: false,
     };
+  },
+  methods: {
+    async sendMail() {
+      const form = new FormData();
+
+      form.append('email', this.email);
+      form.append('mailContent', this.mailContent);
+
+      try {
+        await axios.post(`${process.env.VUE_APP_API_URL}/ag-renovation/send-mail`,
+          form,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+        this.displayValidPopin = true;
+        this.email = '';
+        this.mailContent = '';
+      } catch (e) {
+        console.error(e);
+        this.displayErrorPopin = true;
+      }
+    },
   },
 };
 </script>
